f6f7872d05b349471f3fc0fe010d499f6d68828b
Nick Matthews <matthews3102000@gmail.com>
2023-10-18 21:31:00 +0100

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,6 +1,16 @@
+# This grabs the hostname
 read -p 'hostname:' host_name
 
+# This sets the hostname varirable
 hostnamectl set-hostname $host_name
+
+# This deletes the machine id
 sudo rm -f /etc/machine-id
+
+# This deletes the machine id
 sudo rm -f /var/lib/dbus/machine-id
+
+# This sets up the machine id
 sudo systemd-machine-id-setup
+
+sed -i 's/debian/$host_name/g' /etc/hosts
